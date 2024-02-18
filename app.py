2e03a25dbcc323f933260f4cc597b7b1c35cebf9
Rithesh Akash <ritheshakash006@gmail.com>
2024-02-18 12:01:34 +0530

    New deploy changes

--- app.py
+++ app.py
@@ -1,55 +1,20 @@
 # EMTION PREDICTOR USING TENSORFLOW 
 # Creating the api for sending prediction responses
 
-import contractions
-import pickle
 import os
 import logging
+import sys
+app_dir = os.path.dirname(os.path.abspath(__file__))
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
+sys.path.append(os.path.join(app_dir,"tfcode"))
 
-import tensorflow as tf
-import numpy as np
+from tfcode import prediction
 from flask import Flask,request,jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
-# function to predict the sentiment for given text
-def predict_emotion(text):
-    # loading the model
-    app_dir = os.path.dirname(os.path.abspath(__file__))
-    model_file = os.path.join(app_dir,"model","emotion_predictor.keras")
-    model = tf.keras.models.load_model(model_file)
-
-    # importing training data for creating tokenizer
-    pickle_file = os.path.join(app_dir,"data","tokenizer.pickle")
-    with open(pickle_file,"rb") as handle:
-        tokenizer = pickle.load(handle)
-        
-    # function to preprocess text
-    def preprocess_text(text):
-        text = text.lower()
-        text = contractions.fix(text)
-        sequences = tokenizer.texts_to_sequences([text])
-        padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, maxlen=100)
-        return padded_sequences
-    
-    processed_text = preprocess_text(text)
-    predictions = model.predict(processed_text)
-
-    # function to decode given label
-    def decode_labels(label):
-        decode_table = {0:'joy',1:'anger',2:'love',3:'sadness',4:'fear',5:'surprise'}
-        return decode_table[label]
-        
-
-    predicted_label = np.argmax(predictions, axis=1)[0]
-    sentiment  = decode_labels(predicted_label)
-    prediction_score = np.max(predictions) * 100
-
-    return sentiment,prediction_score
 
 @app.route("/")
 def index():
@@ -58,7 +23,7 @@ def index():
 def predict_emotion_api(): 
     data = request.json
     text = data['text']
-    predicted_emotion,score = predict_emotion(text)
+    predicted_emotion,score = prediction.predict_emotion(text)
     return jsonify({'predicted_emotion': predicted_emotion, 'confidence_percentage':score})
 
 if __name__ == "__main__":
---
+++ tfcode/prediction.py
@@ -0,0 +1,47 @@
+# EMOTION PREDICTION MODEL USING TENSORFLOW
+# Loading the model and testing/making predictions on raw strings
+
+# necessary imports
+import contractions
+import pickle
+import os
+program_files_path = os.environ.get("ProgramFiles")
+os.add_dll_directory(os.path.join(program_files_path,"NVIDIA GPU Computing Toolkit","CUDA","v11.2","bin"))
+
+import tensorflow as tf
+import numpy as np
+app_dir = os.path.dirname(os.path.abspath(__file__))
+
+# function to predict the sentiment for given text
+def predict_emotion(text):
+    # loading the model
+    model_file = os.path.join(app_dir,"model","emotion_predictor.keras")
+    model = tf.keras.models.load_model(model_file)
+
+    # importing training data for creating tokenizer
+    pickle_file = os.path.join(app_dir,"data","tokenizer.pickle")
+    with open(pickle_file,"rb") as handle:
+        tokenizer = pickle.load(handle)
+        
+    # function to preprocess text
+    def preprocess_text(text):
+        text = text.lower()
+        text = contractions.fix(text)
+        sequences = tokenizer.texts_to_sequences([text])
+        padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, maxlen=100)
+        return padded_sequences
+    
+    processed_text = preprocess_text(text)
+    predictions = model.predict(processed_text)
+
+    # function to decode given label
+    def decode_labels(label):
+        decode_table = {0:'joy',1:'anger',2:'love',3:'sadness',4:'fear',5:'surprise'}
+        return decode_table[label]
+        
+
+    predicted_label = np.argmax(predictions, axis=1)[0]
+    sentiment  = decode_labels(predicted_label)
+    prediction_score = np.max(predictions) * 100
+
+    return sentiment,prediction_score
